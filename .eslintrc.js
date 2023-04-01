97b7a1714db51205c137fdc40073991fae6f2121
ytalo azeredo <ytaloazeredo@gmail.com>
2023-04-01 20:26:33 -0300

    fix: eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,19 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
+  extends: 'standard',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
+    'eslint-plugin-import-helpers',
+    '@typescript-eslint/eslint-plugin'
   ],
   rules: {
-    indent: 'off',
-    semi: [2, 'always', { omitLastInOneLineBlock: true }]
+    'no-unused-vars': 'off',
+    'no-useless-constructor': 0
   }
-};
+}
